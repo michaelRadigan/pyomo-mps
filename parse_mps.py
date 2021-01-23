d4ed8d567d4528277df12e30ccefb114c7b7a633
Michael Radigan <michael@radigan.co.uk>
2021-01-23 16:23:13 +0000

    Implement just enough to parse and solve enlight problems

--- parse_mps.py
+++ parse_mps.py
@@ -2,13 +2,15 @@
 # We're going to parse mp files to pyomo
 from enum import Enum
 from pyomo.kernel import *
+import pyomo.core.kernel as pck
 from collections import defaultdict
+import math
 
 # Note to future self, we're going to do this as ugly and fast as possible to start with
 # Completely going for pure filth to just get this done asap.
 # TODO[michaelr]: Come back at some point in the future and make this sane
 
-file_name = r"/Users/michaelradigan/pyomo-mps-parser/mps/enlight9.mps"
+file_name = r"/Users/michaelradigan/pyomo-mps-parser/mps/enlight8.mps"
 
 
 class State(Enum):
@@ -24,7 +26,23 @@ class ConstraintType(Enum):
     E = 1,
     G = 2,
     L = 3,
-    N = 4
+    N = 4,
+
+
+class BoundType(Enum):
+    FR = 1,
+    FX = 2,
+    LO = 3,
+    MI = 4,
+    PL = 5,
+    UP = 6,
+    BV = 7,
+    LI = 8,
+    UI = 9,
+
+
+def to_expr(coefficients, variables):
+    return sum([c*v for c, v in zip(coefficients, variables)])
 
 
 # Dictionary from constraintname to a linear_constraint as defined by the the ROWS section
@@ -35,16 +53,17 @@ constraints = {}
 constraint_count = 0
 
 # dictionary from variable name to a variable. Note that variables in MPs are from 0 to +inf by default
-variables = defaultdict(lambda: variable(lb=0, ub=None))
+variables = defaultdict(lambda: variable(lb=0, ub=None, domain=Integers))
+
+objective = None
 
 
 def parse_row(line):
     constrainttype, name = map(str.strip, line.split())
-
     # This is a bit shit, yes, but we'er just trying to get something to work quickly
     if constrainttype == "E":
         constraint = linear_constraint(
-            variables=[], coefficients=[], terms=None, lb=0, ub=0)
+            variables=[], coefficients=[], terms=None, rhs=0)
         constraints[name] = (constraint, ConstraintType.E)
     elif constrainttype == "G":
         # TODO[michaelr]: We need a way of representing that the lower bound here is finite??
@@ -62,6 +81,9 @@ def parse_row(line):
         constraint = linear_constraint(
             variables=[], coefficients=[], terms=None, lb=None, ub=None)
         constraints[name] = (constraint, ConstraintType.N)
+
+        #objective = pck.objective.objective()
+        #constraints[name] = (objective, ConstraintType.N)
     else:
         raise Exception(
             f"Unknown contstraintType: {constrainttype} for constraint: {name}")
@@ -85,12 +107,10 @@ def parse_column(line):
 
 def parse_rhs(line):
     # Note that we definitely need to be careful here as the MPS definition of RHS amd the
-    # pyomo definition of RHS do not necessarily agree exactly
+    # pyomo definition of RHS do not necessarily agree
 
-    # TODO[michaelr]: implement me
-    print(f"RHS: {line}")
     # We're just going to throw away the vector name, I'm not sure that we have any use for it...
-    vector_name, constraint_name, limit = line.split()
+    _, constraint_name, limit = line.split()
 
     constraint, constraint_type = constraints[constraint_name]
 
@@ -106,16 +126,85 @@ def parse_rhs(line):
         raise Exception('unknown constraint type')
 
 
-def parse_bounds(line):
-    # TODO[michaelr]: implement me
-    print(f"BOUNDS: {line}")
+def parse_fr(variable):
+    """ A free variable, set lower bound to -inf and upper bound to inf """
+    variable.lb = None
+    variable.ub = None
 
 
-parse = {
+def parse_fx(variable, bound_limit):
+    """ A fixed variable, set the lower and upper bounds to the bound_limit """
+    variable.lb = bound_limit
+    variable.ub = bound_limit
+
+
+def parse_lo(variable, bound_limit):
+    """ A lower bound, set the lower bound to bound_limit, the upper bound is unchanged"""
+    variable.lb = bound_limit
+
+
+def parse_mi(variable):
+    """ Set the lower bound to -inf """
+    variable.lb = None
+
+
+def parse_pl(variable):
+    """ Set the upper bound to inf"""
+    variable.ub = None
+
+
+def parse_up(variable, bound_limit):
+    """ An upper bound, set the upper bound to bound_limit, the lower bound is unchanged"""
+    variable.ub = bound_limit
+
+
+def parse_bv(variable):
+    """ Bounds the variable to be a binary variable"""
+    # TODO[michaelr]: This makes the variable integer
+    variable.lb = 0
+    variable.ub = 1
+
+
+def parse_li(variable, bound_limit):
+    """ An integer lower bound, set the lower bound to ceil(bound_limit), the upper bound is unchanged"""
+    variable.lb = math.ceil(bound_limit)
+
+
+def parse_ui(variable, bound_limit):
+    """ An integer upper bound, set the upper bound to floor(bound_limit), the lower bound is unchanged"""
+    variable.ub = math.floor(bound_limit)
+
+
+parse_bound_type = {
+    BoundType.FR: lambda variable, bound_limit: parse_fr(variable),
+    BoundType.FX: lambda variable, bound_limit: parse_fx(variable, bound_limit),
+    BoundType.LO: lambda variable, bound_limit: parse_lo(variable, bound_limit),
+    BoundType.MI: lambda variable, _: parse_mi(variable),
+    BoundType.PL: lambda variable, _: parse_pl(variable),
+    BoundType.UP: lambda variable, bound_limit: parse_up(variable, bound_limit),
+    BoundType.BV: lambda variable, _: parse_bv(variable),
+    BoundType.LI: lambda variable, bound_limit: parse_li(variable, bound_limit),
+    BoundType.UI: lambda variable, bound_limit: parse_ui(variable, bound_limit),
+}
+
+
+def parse_bound(line):
+    # TODO[michaelr]: Some kind of validation
+    # We're throwing away the bound_name, I don't think that we have any use for it
+    bound_key, _, variable_name, bound_limit = line.split()
+
+    variable = variables[variable_name]
+    bound_type = BoundType[bound_key]
+
+    # TODO[michaelr] We're limitting to int here again
+    parse_bound_type[bound_type](variable, int(bound_limit))
+
+
+parse_line = {
     State.ROWS: lambda line: parse_row(line),
     State.COLUMNS: lambda line: parse_column(line),
     State.RHS: lambda line: parse_rhs(line),
-    State.BOUNDS: lambda line: parse_bounds(line)
+    State.BOUNDS: lambda line: parse_bound(line)
 }
 
 current_state = State.START
@@ -125,6 +214,8 @@ with open(file_name) as f:
             continue
         # Match on all of the special cases, otherwise parse using the respective state function
         # TODO[michaelr]: Lots of duplication here that should be cleaned up
+
+        # TODO[michaelr]: We can clean a lot of this by just parsing the raw string into the enum?
         if line.startswith("NAME"):
             if current_state != State.START:
                 raise Exception("NAME is only valid when in the state START")
@@ -147,11 +238,35 @@ with open(file_name) as f:
             if current_state != State.RHS:
                 raise Exception("Bounds is only valid when in the state RHS")
             current_state = State.BOUNDS
-        elif line.startswith("ENDDATA"):
+        elif line.startswith("ENDATA"):
             # TODO[michaelr]: Not sure if we even want any checks here
             break
         else:
-            constraint_count = parse[current_state](line)
+            constraint_count = parse_line[current_state](line)
 
-# print(variables)
-# print(constraints)
+
+# At this point, I think that we may have all of the things that we need, we should build the model!
+model = block()
+model.vd = variable_dict(dict(variables))
+
+# We're doing a fair bit of extra work here
+# There must be a much nicer and more efficient way to do this but it's not really very important for now
+objective = [c for k, (c, t) in constraints.items()
+             if t == ConstraintType.N][0]
+
+
+obj_expr = sum(
+    [c*v for c, v in zip(objective._coefficients, objective._variables)])
+model.o = pck.objective.objective(obj_expr)
+
+
+normal_constaints = {k: c for k, (c, t) in constraints.items()
+                     if t != ConstraintType.N}
+
+if len(normal_constaints) + 1 != len(constraints):
+    raise Exception("We only support having a single objective function")
+model.c = constraint_dict(normal_constaints)
+
+opt = SolverFactory('cbc').solve(model)
+opt.write()
+print(model.o())
