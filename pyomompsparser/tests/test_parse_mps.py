7eba48b7f2800a46c81c62781260518fd051940f
Michael Radigan <michael@radigan.co.uk>
2021-01-24 13:12:23 +0000

    Clean up the tests a bit

--- pyomompsparser/tests/test_parse_mps.py
+++ pyomompsparser/tests/test_parse_mps.py
@@ -1,11 +1,23 @@
 from pyomompsparser import parse
 import pyomo.kernel as pk
+import os
+import pytest
+
+fixture_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'test_files',
+)
+
+enlight8_file_name = 'enlight8.mps'
 
 
-def test_parse():
+@pytest.mark.datafiles(os.path.join(fixture_dir, enlight8_file_name))
+def test_parse_enlight8(datafiles):
     """ very rushed test"""
-    # Utterly shit to have this in the test but it's late on a Saturday night, apologies to future me
-    model = parse(
-        "/Users/michaelradigan/pyomo-mps-parser/mps/enlight8.mps")
+    dir_path = str(datafiles)  # convert from py.path object to a  path
+    assert len(os.listdir(dir_path)) == 1
+    file_path = os.path.join(dir_path, enlight8_file_name)
+    assert os.path.isfile(file_path)
+    model = parse(file_path)
     pk.SolverFactory('cbc').solve(model)
     assert model.o() == 27
